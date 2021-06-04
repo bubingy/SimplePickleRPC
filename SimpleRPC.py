dd6ad3f558d0ce7d93a1e92750b6d4f38ac4ac1f
bubingy <769004837@qq.com>
2021-06-04 14:01:49 +0800

    use async tcp

--- SimpleRPC.py
+++ SimpleRPC.py
@@ -1,8 +1,9 @@
 import sys
-import time
 import pickle
-import socket
+import asyncio
 from typing import Any, Callable
+from functools import wraps
+from asyncio.streams import StreamReader, StreamWriter
 
 
 ##############################
@@ -22,210 +23,163 @@ It contains following keys:
 * function_kwargs - kwargs for function
 '''
 class BaseStreamHandler:
-    def __init__(self) -> None:
-        pass
-
-    @classmethod
-    def send(self, conn: socket.socket, data: Any) -> None:
-        '''Send data to socket connection.
-        
-        :param conn: a socket session.
-        :param return_value: content to send.
-        '''
-        message = b''
-        body = pickle.dumps(data)
-        body_size = len(body)
-        message += body_size.to_bytes(8, 'big')
-        message += body
-        conn.sendall(message)
-
-    @classmethod
-    def receive(self, conn: socket.socket) -> bytes:
-        '''Receive data from socket connection.
-        
-        :param conn: a socket session.
-        :return: retrieved bytes.
-        '''
-        data = b''
-        body_size = int.from_bytes(conn.recv(8), 'big')
-        while body_size > 0:
-            buffer_size = min(4096, body_size)
-            buffer = conn.recv(buffer_size)
-            if not buffer: break
-            data += buffer
-            body_size -= buffer_size
-        return data
-
-    @classmethod
-    def handle_stream(self) -> None:
-        '''To override.
-
-        '''
-        pass
-
-
-class ClientHandler(BaseStreamHandler):
-    '''Default stream handler for client.
+    '''BasicStreamHandler class.
 
     '''
     def __init__(self) -> None:
-        super().__init__()
-
-    @classmethod
-    def handle_stream(self,
-                      conn: socket.socket,
-                      function_name: str,
-                      function_args: tuple=tuple(),
-                      function_kwargs: dict=dict()) -> Any:
-        rpc_request = {
-            'function_name': function_name,
-            'function_args': function_args,
-            'function_kwargs': function_kwargs
-        }
-        self.send(conn, rpc_request)
-        try:
-            data = pickle.loads(self.receive(conn))
-            return data
-        except Exception as e:
-            print(e)
-            return None
-
-
-class ServerHandler(BaseStreamHandler):
-    '''Default stream handler for server.
+        self.__registered_functions = dict()
+        self.__registered_instances = dict()
     
-    '''
-    def __init__(self) -> None:
-        super().__init__()
-
-    @classmethod
-    def handle_stream(self,
-                      conn: socket.socket,
-                      registered_functions: dict,
-                      registered_instances: dict) -> None:
-        rpc_request = pickle.loads(self.receive(conn))
-
-        function_name = rpc_request['function_name']
-        function_args = rpc_request['function_args']
-        function_kwargs = rpc_request['function_kwargs']
-
-        if '.' in function_name:
-            function_call_path = function_name.split('.')
-            assert len(function_call_path) == 2
-
-            instance_name = function_call_path[0]
-            function_name = function_call_path[1]
-            instance_module = sys.modules[registered_instances[instance_name]]
-            method_to_call = getattr(
-                instance_module.__getattribute__(instance_name),
-                function_name
-            )
-            returned_value = method_to_call(
-                *function_args,
-                **function_kwargs
-            )
-        else:
-            method_to_call = getattr(
-                sys.modules[registered_functions[function_name]], 
-                function_name
-            )
-            returned_value = method_to_call(
-                *function_args,
-                **function_kwargs
-            )
-
-        if returned_value is None: return
-        self.send(conn, returned_value)
-
-
-##############################
-#           Server           #
-##############################
-class RPCServer:
-    '''A TCP Server.
-
-    '''
-    def __init__(self, host: str, port: int) -> None:
-        assert isinstance(port, int) and port > 0
-        self.__s = socket.socket(
-            socket.AF_INET,
-            socket.SOCK_STREAM
-        )
-        self.__s.bind((host, port))
-        self.__stream_handler = ServerHandler()
-        self.__functions = dict()
-        self.__instances = dict()
-
-    def set_handler(self, handler) -> None:
-        '''Set stream handler.
-
-        :param handler: stream handler.
-        '''
-        self.__stream_handler = handler
-
     def register_function(self, fun: Callable) -> None:
         '''Register a function for calling.
 
         :param fun: a callable object.
         '''
-        if fun.__name__ in self.__functions.keys():
-            raise 'function already registered.'
-        self.__functions[fun.__name__] = fun.__module__
-
+        @wraps(fun)
+        def decorated():
+            if fun.__name__ in self.__registered_functions.keys():
+                raise 'function already registered.'
+            self.__registered_functions[fun.__name__] = fun.__module__
+        return decorated
+    
     def register_instance(self, instance: object) -> None:
         '''Register a class instance for calling.
 
         :param instance: a class instance.
         '''
         instance_name = get_object_name(instance.__module__, instance)
-        if instance_name in self.__instances.keys():
+        if instance_name in self.__registered_instances.keys():
             raise 'instance already registered.'
-        self.__instances[instance_name] = instance.__module__
+        self.__registered_instances[instance_name] = instance.__module__
 
-    def run_forever(self) -> None:
-        '''Listen port and accept connection.
-
+    @classmethod
+    async def receive(self, reader: StreamReader) -> bytes:
+        '''Receive data.
+            
+        :param reader: a StreamReader object.
+        :return: retrieved bytes.
         '''
-        self.__s.listen(1)
-        print('start listening...')
-        while True:
-            self.__conn, _ = self.__s.accept()
+        data = b''
+        body_size = int.from_bytes(await reader.readexactly(8), 'big')
+        while body_size > 0:
+            buffer_size = min(4096, body_size)
+            buffer = await reader.readexactly(buffer_size)
+            data += buffer
+            body_size -= buffer_size
+        return data
 
-            while True:
-                try:
-                    self.__stream_handler.handle_stream(
-                        self.__conn,
-                        self.__functions,
-                        self.__instances
-                    )
-                except Exception as e:
-                    print(f'{e}, disconnect with client..')
-                    break
+    @classmethod
+    def send(self, writer: StreamWriter, data: Any) -> None:
+        '''Send data.
+        
+        :param writer: a StreamWriter object.
+        :param data: data to send.
+        '''
+        message = b''
+        body = pickle.dumps(data)
+        body_size = len(body)
+        message += body_size.to_bytes(8, 'big')
+        message += body
+        writer.write(message)
+
+
+class BaseServerStreamHandler(BaseStreamHandler):
+    '''BaseServerStreamHandler class
+
+    Please implement following method by yourself:
+        client_connected_cb(self,
+                            reader: StreamReader,
+                            writer: StreamWriter) -> Any
+    '''
+    async def client_connected_cb(self,
+                                  reader: StreamReader,
+                                  writer: StreamWriter) -> Any:
+        '''This method will be called if a connection is established. 
+
+        :param reader: a StreamReader object.
+        :param writer: a StreamWriter object.
+        '''
+        pass
+
+
+class BaseClientStreamHandler(BaseStreamHandler):
+    '''BaseClientStreamHandler class
+
+    Please implement following method by yourself:
+        communicate_with_server(self) -> None
+    '''
+    def __init__(self) -> None:
+        super().__init__()
+
+    async def connect_to_server(self, host: str, port: int) -> None:
+        self.reader, self.writer = await asyncio.open_connection(host, port)
+
+    async def communicate_with_server(self) -> None:
+        pass
+
+
+##############################
+#           Server           #
+##############################
+class RPCServer:
+    '''A basic async RPC server.
+
+    '''
+    def __init__(self, handler: BaseServerStreamHandler) -> None:
+        '''Constructor.
+
+        :param handler: a BaseServerStreamHandler object which 
+            implement client_connected_cb method.
+        '''
+        self.handler = handler
+
+    def serve_forever(self,
+                      host: str,
+                      port: int,
+                      **kwargs) -> None:
+        '''Start the server.
+        
+        :param host: host ip.
+        :param port: port to listen.
+        '''
+        # get event loop
+        loop = kwargs.get('loop')
+        if loop is not None: 
+            self.event_loop = loop
+            asyncio.set_event_loop()
+        else:
+            self.event_loop = asyncio.get_event_loop()
+
+        # create async TCP server coroutine 
+        self.server_coro = asyncio.start_server(
+            self.handler.client_connected_cb,
+            host,
+            port,
+            **kwargs
+        )
+        self.event_loop.run_until_complete(self.server_coro)
+        self.event_loop.run_forever()
 
 
 ##############################
 #           Client           #
 ##############################
 class RPCClient:
-    def __init__(self, host: str, port: int) -> None:
-        assert isinstance(port, int) and port > 0
-        self.__s = socket.socket(
-            socket.AF_INET,
-            socket.SOCK_STREAM
-        )
-        self.__s.connect((host, port))
-        self.__stream_handler = ClientHandler()
+    '''A basic async RPC client.
 
-    def call(self,
-             function_name: str,
-             function_args: tuple=tuple(),
-             function_kwargs: dict=dict()) -> Any:
-        return self.__stream_handler.handle_stream(
-            self.__s,
-            function_name,
-            function_args,
-            function_kwargs
-        )
+    '''
+    def __init__(self, handler: BaseClientStreamHandler) -> None:
+        '''Constructor.
+
+        :param handler: a BaseClientStreamHandler object which 
+            implement client_connected_cb method.
+        '''
+        self.handler = handler
+
+    def start_communicate(self):
+        asyncio.run(self.handler.connect_to_server())
+        asyncio.run(self.handler.communicate_with_server())
+
 
 ##############################
 #           Utils            #
